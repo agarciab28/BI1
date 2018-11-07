7ac3e96b675377d2ff92a563114fec9beeb194bb
Alejandro Garcia Barajas <agarciab28@gmail.com>
2018-11-06 20:31:31 -0600

    correcion registro.php

--- paginas/registro.php
+++ paginas/registro.php
@@ -1,7 +1,7 @@
 <?php
 //codigo para el registro de un usuario
   $db="proyis";
-  $con=mysqli_connect("localhost","proyis","123456",$db) or die("conexion fallida");
+  $con=mysqli_connect("localhost","rootis","123456",$db) or die("conexion fallida");
   //adquisicion de todos los campos
   $nombre=$_POST["txtNombre"];
   $apellidos=$_POST["txtApellidos"];
@@ -14,6 +14,23 @@
   $curp=$_POST["txtCurp"];
 
   //Inserccion de usuario
-  $sql = mysqli_query($con,"INSERT INTO info_usuario ('usuario', 'clave', 'nombre', 'apellidos', 'correo', 'fechaNac', 'nivelEstudios', 'ciudad', 'curp', 'perfil') VALUES ('$usuario', '$pass', '$nombre', '$apellidos', '$correoElectronico', '$fechaNacimiento', '$estudios', '$ciudad', '$curp', '1')");
+  $sql = mysqli_query($con,"INSERT INTO info_usuario VALUES ('$usuario', '$pass', '$nombre', '$apellidos', '$correoElectronico', '$fechaNacimiento', '$estudios', '$ciudad', '$curp', 1)");
+
+  if ($sql) {
+    header("Location: ../index.php");
+  }
+
+  else {
+    echo $nombre;
+    echo $apellidos;
+    echo $usuario;
+    echo $correoElectronico;
+    echo $pass;
+    echo $fechaNacimiento;
+    echo $estudios;
+    echo $ciudad;
+    echo $curp;
+    //echo "<script type='text/javascript'>alert('No fue posible el registro');window.location = '../index.php';</script>";
+  }
 
   ?>
