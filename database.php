daf12be6c8baeb32222ac1bcdee4166786352311
Alejandro Garcia Barajas <agarciab28@gmail.com>
2018-11-08 23:51:06 -0600

    eliminacion prueba

--- database.php
+++ database.php
@@ -3,5 +3,5 @@
 $db='proyis';
 $con=mysql_connect('localhost','rootis','123456') or die('Problema con la conexion');
 mysql_select_db($db,$con)or die('Problema con la base de datos');
-Prueba de algo 
+
  ?>
