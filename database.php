36e5a7bfe8f2981fa7a4abf41b30cfaba2ae5ad6
JJMRTZ <juan_nadador21@hotmail.com>
2018-11-10 16:34:50 -0600

    Documentacion

--- database.php
+++ database.php
@@ -1,5 +1,7 @@
-<!--Coneccion a la base de datos-->
 <?php 
+/**
+*Conexión a la base de datos
+*/
 $db='proyis';
 $con=mysql_connect('localhost','rootis','123456') or die('Problema con la conexion');
 mysql_select_db($db,$con)or die('Problema con la base de datos');
--- paginas/validar.php
+++ paginas/validar.php
@@ -1,19 +1,27 @@
 <?php 
-//Codigo para la validacion de usuario y contraseña
+/**
+*Codigo para la validacion de usuario y contraseña
+*/
 $db='proyis';
 $con=mysqli_connect('localhost','rootis','123456',$db) or die('Problema con la conexion');
 $c=0;
 
-//Variables del usuario
+/**
+*Variables del usuario
+*/
 $usuario=$_POST['txtUsuario'];
 $pass=$_POST['txtPassword'];
-//Validar contenido en las variables o cajas de texto
+/**
+*Validar contenido en las variables o cajas de texto
+*/
 if(empty($usuario)|empty($pass))
 {
 	header("Location: ../index.php");
 	exit();
 }
-//Validacion Existencia del usuario
+/**
+*Validacion Existencia del usuario
+*/
 $sql = mysqli_query($con,"SELECT * FROM info_usuario where usuario = '$usuario' and clave='$pass'");
 
 if ($row = mysqli_fetch_array($sql)) {
@@ -27,7 +35,9 @@ else
 	echo"<a href='../index.php'>Volver</a>"; 
 	$c=1; 
 } 
-//Validacion Profesionista
+/**
+* Validacion Profesionista
+*/
 $sql2= mysqli_query($con,"SELECT * FROM info_profesionista where usuario = '$usuario' and clave='$pass'");
 if ($row = mysqli_fetch_array($sql2)) {
 	session_start();
