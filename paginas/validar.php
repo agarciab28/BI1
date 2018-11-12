42cb37057febd4af21a2f6c417cc0fa240f2698f
Alejandro Garcia Barajas <agarciab28@gmail.com>
2018-11-11 21:51:29 -0600

    login md5

--- paginas/validar.php
+++ paginas/validar.php
@@ -1,23 +1,19 @@
-<?php 
+<?php
 //Codigo para la validacion de usuario y contraseña
 $db='proyis';
 $con=mysqli_connect('localhost','rootis','123456',$db) or die('Problema con la conexion');
 $c=0;
 
-/**
-*Variables del usuario
-*/
+//Variables del usuario
 $usuario=$_POST['txtUsuario'];
-$pass=$_POST['txtPassword'];
+$pass=md5($_POST['txtPassword']);
 //Validar contenido en las variables o cajas de texto
 if(empty($usuario)|empty($pass))
 {
 	header("Location: ../index.php");
 	exit();
 }
-/**
-*Validacion Existencia del usuario
-*/
+//Validacion Existencia del usuario
 $sql = mysqli_query($con,"SELECT * FROM info_usuario where usuario = '$usuario' and clave='$pass'");
 
 if ($row = mysqli_fetch_array($sql)) {
@@ -25,12 +21,12 @@ if ($row = mysqli_fetch_array($sql)) {
 	$_SESSION['usuario'] = $usuario;
 	header("Location: ../perfil.php");
 }
-else 
-{ 
-	echo "<script>alert('Usuario o contraseña incorrectas')</script>"; 
-	echo"<a href='../index.php'>Volver</a>"; 
-	$c=1; 
-} 
+else
+{
+	echo "<script>alert('Usuario o contraseña incorrectas')</script>";
+	echo"<a href='../index.php'>Volver</a>";
+	$c=1;
+}
 //Validacion Profesionista
 $sql2= mysqli_query($con,"SELECT * FROM info_profesionista where usuario = '$usuario' and clave='$pass'");
 if ($row = mysqli_fetch_array($sql2)) {
